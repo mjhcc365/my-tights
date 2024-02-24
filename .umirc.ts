e4e9067ad351a8c245c177de16fbd7d146990bdc
胡子银 <huziyin@huziyindeMacBook-Air.local>
2024-02-24 11:08:47 +0800

    fix:  删除dmg依赖

--- .umirc.ts
+++ .umirc.ts
@@ -3,11 +3,11 @@ import { Platform, Arch } from '@umijs/plugin-electron';
 
 export default defineConfig({
   npmClient: 'yarn',
-  plugins: ['@umijs/plugin-electron'],
+  // plugins: ['@umijs/plugin-electron'],
   favicons: ["/favicon.svg"],
-  electron: {
-    builder: {
-      targets: Platform.MAC.createTarget(['dmg'], Arch.arm64),
-    },
-  }
+  // electron: {
+  //   builder: {
+  //     targets: Platform.MAC.createTarget(['dmg'], Arch.arm64),
+  //   },
+  // }
 });
