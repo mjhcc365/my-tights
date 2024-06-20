a0665014675f7d05865405209993cd767713b3cc
mjhcc365 <2209011911@qq.com>
2024-06-20 10:32:46 +0800

    test mobx6 计数器

--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -1,33 +1,68 @@
 // import
 import { Button } from "antd";
-import { useEffect, useRef } from "react";
-import * as fabric from "fabric";
-// import { options } from "./mockdata";
+import { observer } from "mobx-react-lite";
+import { action, makeAutoObservable, makeObservable, observable } from "mobx";
+import { useEffect, useRef, createContext, useContext } from "react";
 
-const TestPage = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const ref = useRef<fabric.Canvas | undefined>(undefined);
-  const handleClick = () => {
-    console.log("===>");
-  };
-
-  useEffect(() => {
-    const newCanvas = new fabric.Canvas(canvasRef.current as any, {
-      width: 1000,
-      height: 500,
+class CounterStore {
+  // 数值状态
+  count = 10;
+  constructor() {
+    // makeAutoObservable(this) 也好使
+    makeObservable(this, {
+      count: observable,
+      increment: action,
+      reset: action,
     });
-    ref.current = newCanvas;
+  }
+  // 使数值状态加一
+  increment() {
+    this.count += 1;
+  }
+  // 重置数值状态
+  reset() {
+    this.count = 0;
+  }
+}
 
-    return () => {
-      ref.current?.dispose();
-    };
-  }, []);
+const store = new CounterStore();
+
+const TimerContext = createContext<CounterStore>(null);
+
+const ValueCom = observer(() => {
+  const store = useContext(TimerContext);
+  return <div>{store.count}</div>;
+});
+
+const Option = observer(() => {
+  const store = useContext(TimerContext);
   return (
     <div>
-      <Button onClick={handleClick}>添加table</Button>
-      <canvas ref={canvasRef} />
+      <Button
+        onClick={() => {
+          store.increment();
+        }}
+      >
+        +
+      </Button>
+      <Button
+        onClick={() => {
+          store.reset();
+        }}
+      >
+        reset
+      </Button>
     </div>
   );
-};
+});
 
-export default TestPage;
+const Counter = observer(() => {
+  return (
+    <TimerContext.Provider value={store}>
+      <ValueCom />
+      <Option />
+    </TimerContext.Provider>
+  );
+});
+
+export default Counter;
