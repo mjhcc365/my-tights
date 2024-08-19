c91b435ff3902e6357e93459f7dd3265ce1a78a6
mjhcc365 <2209011911@qq.com>
2024-08-19 17:38:10 +0800

    feat: 添加贴纸

--- src/pages/EditPage/components/PictureSection/Sticker.tsx
+++ src/pages/EditPage/components/PictureSection/Sticker.tsx
@@ -1,6 +1,35 @@
 // 实现懒加载
+
+import { FabricImage } from "fabric";
+import { useContext } from "react";
+import { CanvasStoreContext } from "@/store/canvas";
+
+// const stiker = import("@/assets/stickers/weeknote.jpg");
+
+import stiker from "@/assets/stickers/weeknote.jpg";
+
 const Sticker = () => {
-  return <div>Sticker</div>;
+  const store = useContext(CanvasStoreContext);
+
+  const handleAddPicture = async () => {
+    FabricImage.fromURL(stiker).then((img) => {
+      img.scaleToWidth(100); // 设置图像宽度
+      img.scaleToHeight(100); // 设置图像高度
+      store.addObject(img); // 添加图像到 Canvas
+    });
+  };
+
+  return (
+    <div>
+      <img
+        src={stiker}
+        onClick={() => {
+          handleAddPicture();
+        }}
+      />
+      Sticker
+    </div>
+  );
 };
 
 export default Sticker;
